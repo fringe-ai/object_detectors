a1daa2048f516ed0796fa1d2213f0aa55e466e48
Rugved Mavidipalli <rmavidipalli@gmail.com>
2024-12-16 09:13:34 -0500

    Faie 2110 (#100)
    * ultralytics yolo support
    
    * auto start tensorboard
    
    * updated docs
    
    * added readme
    
    * added readme
    
    * updated test cases
    
    * backport support instead of deprecating
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * adding support for resize and pad
    
    * default startup for tensorboard
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * reverting
    
    * updated a model
    
    * updated implimentation
    
    * updated test
    
    * small updated
    
    * changed api to previous

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@
 import subprocess
 import pytest
 import logging
+import os
 import sys
 
 logging.basicConfig()
@@ -22,7 +23,10 @@ def pytest_configure(config):
     try:
         logger.info('Starting git LFS pull...')
         result = subprocess.run(["git", "lfs", "pull"], check=True, capture_output=True, text=True)
-        logger.info(result.stdout)
+        logger.info(f"Git LFS pull output: {result.stdout}")
     except subprocess.CalledProcessError as e:
-        logger.exception(f"Error running git lfs pull: {e.stderr}")
-        pytest.exit("Exiting tests due to git lfs pull failure")
+        logger.exception(f"Error running git lfs pull some tests may fail: {e.output}")
+        if "Not in a Git repository" in e.output:
+            logger.error("You are not in a git repository. Please run this command in a git repository.")
+        else:
+            pytest.exit(f"Error running git lfs pull some tests may fail: {e.output}")
