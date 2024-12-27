668c5589fb6a3a9f91c31eb029e4ae22d8f83d52
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-12-26 21:35:53 -0500

    removed vars

--- object_detectors/detectron2_lmi/convert.py
+++ object_detectors/detectron2_lmi/convert.py
@@ -3,11 +3,6 @@ import subprocess
 from detectron2_lmi.converter.detectron2_exporter import det2export
 from detectron2_lmi.converter.detectron2_onnx_trtonnx import onnx_gs
 
-DET2_ONNX_EXPORT = 'model.onnx'
-DET2_TRT_EXPORT = 'model.engine'
-DET2_PT_EXPORT = 'model.pt'
-DET2_DEFAULT_DIR = '/home/weights'
-
 def convert_to_trt(args):
     command = f"trtexec --onnx={args.get('onnx_file_path')} --saveEngine={args.get('trt_file_path')} --useCudaGraph"
     if args.get('fp16', False):
